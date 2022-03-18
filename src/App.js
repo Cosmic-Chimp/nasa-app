aed63e92e0cee68352c837a7e3b5e5659e4050c5
Rueben <rubzdebruyn@gmail.com>
2022-03-18 13:31:10 +0200

    react-helmet-added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import Home from "./components/Home";
 import NasaPic from "./components/NasaPic";
 import "./Sass/main.scss";
 import { AnimatePresence } from "framer-motion";
+import { Helmet } from "react-helmet";
 function App() {
   const location = useLocation();
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Tic-Tac-UAP</title>
+        <meta name="description" content="Tic Tac Ufo/UAP site" />
+        {/* <link rel="canonical" href="http://mysite.com/example" /> */}
+      </Helmet>
+
+      {/*  */}
       <AnimatePresence exitBeforeEnter>
         <Switch location={location} key={location.key}>
           {/* <div> */}
